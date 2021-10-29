a110c371dc0baa4853eb93c76416f460d0b7590f
arista <atd-help@arista.com>
2021-10-29 17:07:18 +0000

    Updated builder

--- configlet_builder/generate_underlay.py
+++ configlet_builder/generate_underlay.py
@@ -22,18 +22,86 @@ if rest_client.connect():
 
 underlay = yaml.safe_load(raw['config'])
 
+prefix_list = """
+ip prefix-list LOOPBACK
+    seq 10 permit 192.168.101.0/24 eq 32
+    seq 20 permit 192.168.102.0/24 eq 32
+    seq 30 permit 192.168.201.0/24 eq 32
+    seq 40 permit 192.168.202.0/24 eq 32
+    seq 50 permit 192.168.253.0/24 eq 32
+"""
+route_map = """
+route-map LOOPBACK permit 10
+   match ip address prefix-list LOOPBACK
+"""
 
+peer_filter = """
+peer-filter LEAF-AS-RANGE
+ 10 match as-range 65000-65535 result accept
+"""
 
 # Global Variables
 
 mtu = underlay['global']['MTU']
 
-for interface in underlay[hostname]['interfaces']:
-  ip = underlay[hostname]['interfaces'][interface]['ipv4']
-  mask = underlay[hostname]['interfaces'][interface]['mask']
-  print("interface %s") % interface
-  print("   ip address %s/%s") % (ip, mask)
-  if 'Ethernet' in interface:
-    print("   no switchport")
-    print("   mtu %s") % mtu
+def generate_interfaces():
+  for interface in underlay[hostname]['interfaces']:
+    ip = underlay[hostname]['interfaces'][interface]['ipv4']
+    mask = underlay[hostname]['interfaces'][interface]['mask']
+    print("interface %s") % interface
+    print("   ip address %s/%s") % (ip, mask)
+    if 'Ethernet' in interface:
+      print("   no switchport")
+      print("   mtu %s") % mtu
+      
+def generate_leaf_BGP():
+  loopback0 = underlay[hostname]['interfaces']['loopback0']['ipv4']
+  ASN = underlay[hostname]['BGP']['ASN']
+  spine_ASN = underlay['global']['DC1']['spine_ASN']
 
+  print(prefix_list)
+  print(route_map)
+  print("router bgp %s") % ASN
+  print("   router-id %s") % loopback0
+  
+  print("   neighbor Underlay peer group")
+  print("   neighbor Underlay remote-as %s") % spine_ASN
+  print("   neighbor Underlay send-community")
+  print("   neighbor Underlay maximum-routes 12000")
+  
+  spine_peers = underlay[hostname]['BGP']['spine-peers']
+  for spine in spine_peers:
+    print("    neighbor %s peer group Underlay") % spine
+  
+  print("    address-family ipv4")
+  print("       neighbor Underlay activate")
+  print("       redistribute connected route-map LOOPBACK")
+  
+def generate_spine_BGP():
+  loopback0 = underlay[hostname]['interfaces']['loopback0']['ipv4']
+  ASN = underlay[hostname]['BGP']['ASN']
+  print(peer_filter)
+  print(prefix_list)
+  print(route_map)
+  print("router bgp %s") % ASN
+  print("   router-id %s") % loopback0
+  print("   no bgp default ipv4-unicast")
+  print("   maximum-paths 3")
+  print("   distance bgp 20 200 200")
+  print("   bgp listen range 192.168.103.0/24 peer-group LEAF_Underlay peer-filter LEAF-AS-RANGE")
+  print("   neighbor LEAF_Underlay peer group")
+  print("   neighbor LEAF_Underlay send-community")
+  print("   neighbor LEAF_Underlay maximum-routes 12000")
+  print("   address-family ipv4")
+  print("   neighbor LEAF_Underlay activate")
+  print("   redistribute connected route-map LOOPBACK")
+  
+
+  
+      
+generate_interfaces()
+
+if 'leaf' in hostname:
+  generate_leaf_BGP()
+if 'spine' in hostname:
+  generate_spine_BGP()
